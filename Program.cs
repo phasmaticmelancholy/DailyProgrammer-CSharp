a525fb74611ee5b04d00dd3e387c1372194cbb86
phasmaticmelancholy <phasmaticmelancholy@gmail.com>
2023-09-12 23:00:22 -0400

    Cleanup

--- Program.cs
+++ Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//https://old.reddit.com/r/dailyprogrammer/comments/onfehl/20210719_challenge_399_easy_letter_value_sum/
+string alphabet = "abcdefghijklmnopqrstuvwxyz";
+string[] words = File.ReadAllLines("../../../enable1.txt");
+
+WordToNumber("");
+WordToNumber("a");
+WordToNumber("z");
+WordToNumber("cab");
+WordToNumber("excellent");
+WordToNumber("microspectrophotometries");
+Bonus1();
+
+int WordToNumber(string aWord, bool print=true)
+{
+    int total = 0;
+    foreach(char letter in aWord.ToCharArray())
+    {
+        total += alphabet.IndexOf(letter)+1;
+    }
+
+    if(print)
+    {
+        Console.WriteLine($"'{aWord}' => {total}");
+    }
+
+    return total;
+}
+
+void Bonus1()
+{
+    foreach(string word in words)
+    {
+        if(WordToNumber(word, false) == 319)
+        {
+            Console.WriteLine($"'{word}' => 319");
+            break;
+        }
+    }
+}
