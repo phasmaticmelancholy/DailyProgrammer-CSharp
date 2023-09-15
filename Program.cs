20ed6b6bd594dcdf9961f46591af839236325156
phasmaticmelancholy <phasmaticmelancholy@gmail.com>
2023-09-14 23:04:51 -0400

    More bonus questions

--- Program.cs
+++ Program.cs
@@ -5,6 +5,33 @@ using System.Collections.Immutable;
 using System.Data;
 
 string alphabet = "abcdefghijklmnopqrstuvwxyz";
+Dictionary<char, int> letterToNumber = new Dictionary<char, int>{
+    {'a', 1}, 
+    {'b', 2},
+    {'c', 3},
+    {'d', 4},
+    {'e', 5},
+    {'f', 6},
+    {'g', 7},
+    {'h', 8},
+    {'i', 9},
+    {'j', 10},
+    {'k', 11},
+    {'l', 12},
+    {'m', 13},
+    {'n', 14},
+    {'o', 15},
+    {'p', 16},
+    {'q', 17},
+    {'r', 18},
+    {'s', 19},
+    {'t', 20},
+    {'u', 21},
+    {'v', 22},
+    {'w', 23},
+    {'x', 24},
+    {'y', 25},
+    {'z', 26}};
 string[] words = File.ReadAllLines("../../../enable1.txt");
 
 //0
@@ -22,6 +49,8 @@ WordToNumber("microspectrophotometries");
 Bonus1();
 Bonus2();
 Bonus3();
+Bonus4();
+Bonus5();
 
 //Convert a provided String to a number that comes from the sum of all of the letters in that String, assuming each
 //letter has a value that corresponds to its position in the alphabet
@@ -30,7 +59,7 @@ int WordToNumber(string aWord, bool print=true)
     int total = 0;
     foreach(char letter in aWord.ToCharArray())
     {
-        total += alphabet.IndexOf(letter)+1;
+        total += letter - 96;
     }
 
     if(print)
@@ -95,12 +124,52 @@ void Bonus3()
 
 void Bonus4()
 {
-    //TODO
+    for(int i=0; i<words.Length; i++)
+    {
+        int letterSum = WordToNumber(words[i], false);
+        int length = words[i].Length;
+        for(int j=i+1; j<words.Length; j++)
+        {
+            if(Math.Abs(length - words[j].Length) != 11) continue;
+
+            if(letterSum == WordToNumber(words[j], false))
+            {
+                Console.WriteLine($"{words[i]} has the same letter sum as {words[j]}");
+                return;
+            }
+        }
+    }
 }
 
 void Bonus5()
 {
-    //TODO
+    int matchCount = 0;
+    for(int i=0; i<words.Length; i++)
+    {
+        if(matchCount == 2) break;
+        int letterSum = WordToNumber(words[i], false);
+        
+        for(int j=i+1; letterSum > 188 && j<words.Length; j++)
+        {
+            if(matchCount == 2) break;
+            if(letterSum == WordToNumber(words[j], false))
+            {
+                int k=0;
+
+                foreach(char letterI in words[i].ToCharArray())
+                {
+                    if(words[j].Contains(letterI)) break;
+                    k++;
+                }
+
+                if(k == words[i].Length)
+                {
+                    matchCount++;
+                    Console.WriteLine($"{words[i]} has the same letter sum as {words[j]} and no common letters");
+                }
+            }
+        }     
+    }
 }
 
 void Bonus6()
